ff6bbcff23b33aefe3d6611faaf7358b2bda9d30
В'ячеслав <suzium.trap@gmail.com>
2023-10-09 23:13:59 +0300

    Added RealFileManager class

---
+++ Artificial/RealFileManager.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "SectorName.h"
+#include <string>
+#include <vector>
+
+class RealFileManager final
+{
+public:
+	RealFileManager(uint8_t _key) : m_key(_key) {}
+
+	void write_into_real_file(SectorName _name, const std::vector<uint8_t>& _raw);
+	std::vector<uint8_t> read_from_real_file(SectorName _name);
+	void delete_real_file(SectorName _name);
+
+private:
+	uint8_t m_key;
+};
