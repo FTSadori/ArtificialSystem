3b7b5cc80163fd4ef7ae6c333c515a9d10c42d09
В'ячеслав <suzium.trap@gmail.com>
2023-10-17 18:08:05 +0300

    Added DataQueue class

---
+++ Artificial/DataQueue.cpp
@@ -0,0 +1,44 @@
+#include "DataQueue.h"
+
+namespace Core::Memory
+{
+	void DataQueue::push_char(char c)
+	{
+		m_data.push_back(c);
+	}
+
+	void DataQueue::fix_last_char()
+	{
+		m_data.pop_back();
+	}
+
+	const char* DataQueue::get_data() const
+	{
+		return m_data.data();
+	}
+
+	size_t DataQueue::size() const
+	{
+		return m_data.size();
+	}
+
+	void DataQueue::concat(const DataQueue& _other)
+	{
+		m_data.insert(m_data.end(), _other.m_data.begin(), _other.m_data.end());
+	}
+
+	char DataQueue::operator[](size_t index) const
+	{
+		return m_data[index];
+	}
+
+	DataQueue::DataQueue(const std::vector<char>& vec)
+	{
+		concat_vector(vec);
+	}
+
+	void DataQueue::concat_vector(const std::vector<char>& vec)
+	{
+		m_data.insert(m_data.end(), vec.begin(), vec.end());
+	}
+}
---
+++ Artificial/DataQueue.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "DataQueueExceptions.h"
+#include <vector>
+
+namespace Core::Memory
+{
+	class DataQueue final
+	{
+	public:
+		DataQueue() {}
+		DataQueue(const std::vector<char>& vec);
+
+		void concat_vector(const std::vector<char>& vec);
+
+		void push_char(char c);
+		void fix_last_char();
+		
+		const char* get_data() const;
+
+		char operator[](size_t index) const;
+		size_t size() const;
+
+		void concat(const DataQueue& _other);
+
+		template<typename value_t> void push(const value_t& val)
+		{
+			m_data.insert(m_data.end(), (char*)&val, (char*)&val + sizeof(value_t));
+		}
+
+		template<typename value_t> value_t pop()
+		{
+			if (m_data.size() < sizeof(value_t))
+				throw NotEnoughDataInStream("Data in stream is not enough to fill value");
+			std::vector<char> first_part(m_data.begin(), m_data.begin() + sizeof(value_t));
+			value_t val = *((value_t*)first_part.data());
+			m_data.erase(m_data.begin(), m_data.begin() + sizeof(value_t));
+			return val;
+		}
+
+	private:
+		std::vector<char> m_data;
+	};
+}
