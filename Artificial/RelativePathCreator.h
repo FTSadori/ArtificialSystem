be056ef6069f4eb94331877cb7413eb59c7e054e
В'ячеслав <suzium.trap@gmail.com>
2023-11-30 21:43:54 +0200

    Added RelativePathCreator

---
+++ Artificial/RelativePathCreator.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "DiskPath.h"
+#include "FullPath.h"
+#include "Separator.h"
+
+namespace Memory
+{
+	class RelativePathCreator final
+	{
+	public:
+		static FullPath combine(const std::string& from, const std::string& path)
+		{
+			if (path.find(FullPath::c_disk_div) < path.size())
+				return FullPath(path);
+			FullPath from_path(from);
+			DiskPath result(from_path.disk_path());
+
+			auto list = Commands::Separator::split(path, '\\');
+			for (const auto& l : list)
+			{
+				if (l == "..")
+					result = DiskPath(result.dir());
+				else
+					result = DiskPath(result.full_name(), l);
+			}
+
+			return FullPath(from_path.mark(), result);
+		}
+	};
+}
