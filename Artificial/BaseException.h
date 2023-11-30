5dd151d48a7c8c783c35d577ecd2e1b4b9427487
В'ячеслав <suzium.trap@gmail.com>
2023-11-30 19:17:06 +0200

    Removed BaseException from Memory namespace

--- Artificial/BaseException.h
+++ Artificial/BaseException.h
@@ -2,16 +2,13 @@
 #include <exception>
 #include <string>
 
-namespace Memory
+class Exception : public std::exception
 {
-	class Exception : public std::exception
-	{
-	public:
-		Exception(const std::string& _message) : m_message(_message) {}
-		Exception(const char* _message) : m_message(_message) {}
-		virtual const char* what() const { return m_message.c_str(); }
+public:
+	Exception(const std::string& _message) : m_message(_message) {}
+	Exception(const char* _message) : m_message(_message) {}
+	virtual const char* what() const { return m_message.c_str(); }
 
-	protected:
-		std::string m_message;
-	};
-}
+protected:
+	std::string m_message;
+};
