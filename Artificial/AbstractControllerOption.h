51da95444152a0419290f5d5841c4aa649cd91c9
В'ячеслав <suzium.trap@gmail.com>
2023-12-05 15:53:29 +0200

    Made controller option abstract

---
+++ Artificial/AbstractControllerOption.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "ICommand.h"
+#include "ISender.h"
+#include "DiskSystem.h"
+#include "SystemSender.h"
+#include "ICommandExecutor.h"
+
+namespace Commands
+{
+	class AbstractControllerOption
+	{
+	public:
+		AbstractControllerOption(ICommandExecutor& _core)
+			: m_core(_core) {}
+
+		virtual std::string execute(const ICommand& command, const ISender& sender) = 0;
+		
+		virtual std::string send(const ICommand& command)
+		{
+			return m_core.execute(command, SystemSender());
+		}
+
+		virtual ~AbstractControllerOption() = default;
+
+	private:
+		ICommandExecutor& m_core;
+	};
+}
