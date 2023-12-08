13ef1537c5cc637ad0e7c7bef6db558a5d0d3ebf
В'ячеслав <suzium.trap@gmail.com>
2023-12-08 15:52:14 +0200

    GUI: Removed colours from ScreenText

--- Artificial/ScreenText.h
+++ Artificial/ScreenText.h
@@ -17,25 +17,13 @@ namespace GUI
 	{
 	public:
 		ScreenText(Size _size)
-			: c_size(_size),
-			  c_default_back(Colours::BLACK), c_default_fore(Colours::WHITE),
-			  c_secondary_fore(Colours::GRAY)
-		{}
-
-		ScreenText(Size _size, Colours _back, Colours _fore, Colours _secondary)
-			: c_size(_size),
-			c_default_back(_back), c_default_fore(_secondary),
-			c_secondary_fore(_fore)
+			: c_size(_size)
 		{}
 
 		void push_text(const std::string& text, TextAttributes attributes);
 		void render_text_from(ScreenPoint absolute, size_t line_num);
 
 	private:
-
-		const Colours c_default_back;
-		const Colours c_default_fore;
-		const Colours c_secondary_fore;
 		const Size c_size;
 		std::vector<std::string> m_text;
 		std::map<size_t, std::vector<AttributePoint>> m_point_map;
