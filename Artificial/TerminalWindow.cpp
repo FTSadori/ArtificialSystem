a6f77d7fea59e63e607870de3853a589955c4de8
В'ячеслав <suzium.trap@gmail.com>
2023-12-13 15:40:08 +0200

    GUI: Updated TerminalWindow

--- Artificial/TerminalWindow.cpp
+++ Artificial/TerminalWindow.cpp
@@ -10,75 +10,117 @@ namespace GUI
 		start_new_command();
 	}
 
+	void TerminalWindow::print_main(const std::string& line)
+	{
+		print(line, m_main);
+	}
+
+	void TerminalWindow::print_secondary(const std::string& line)
+	{
+		print(line, m_secondary);
+	}
+
+	void TerminalWindow::print_third(const std::string& line)
+	{
+		print(line, m_third);
+	}
+
+	void TerminalWindow::print(const std::string& line, TextAttributes attributes)
+	{
+		if (m_caninput) return;
+
+		std::lock_guard lock(m_text_mutex);
+		m_text_parts.emplace_back(line, attributes);
+	}
+
+	void TerminalWindow::wait_for_input(TerminalInputType type)
+	{
+		if (m_caninput) return;
+	
+		std::unique_lock lock(m_text_mutex);
+		m_caninput = true;
+		m_current_input_type = type;
+		m_cv.wait(lock, [&] { return m_entered; });
+	}
+
+	std::string TerminalWindow::get_last_input()
+	{
+		std::lock_guard lock(m_text_mutex);
+		m_entered = false;
+		return m_last_input;
+	}
+
 	void TerminalWindow::key_pressed(KEY_EVENT_RECORD key_event)
 	{
-		SHORT code = key_event.uChar.AsciiChar;
-		
-		if (!m_canprint)
-		{
-			if (code >= 32 && code <= 126)
-			{
-				m_buffer.back() = m_buffer.back().substr(0, m_cursor_in_buffer) 
-					+ CHAR(code) 
-					+ m_buffer.back().substr(m_cursor_in_buffer);
-				++m_cursor_in_buffer;
-				render_background();
-				render_text_end();
-				return;
-			}
+		if (!m_caninput) { return; }
 
-			ScreenText text(m_size);
+		SHORT code = key_event.uChar.AsciiChar;
+
+		// printable symbol
+		if (code >= 32 && code <= 126)
+		{
+			m_last_input = m_last_input.substr(0, m_cursor_in_input)
+				+ CHAR(code)
+				+ m_last_input.substr(m_cursor_in_input);
+			++m_cursor_in_input;
+		}
+		else
+		{
 			switch (key_event.wVirtualKeyCode)
 			{
+			// backspace
 			case 8:
-				if (m_buffer.back().size() != 0)
+				if (m_last_input.size() != 0)
 				{
-					m_buffer.back() = m_buffer.back().substr(0, m_cursor_in_buffer - 1)
-						+ m_buffer.back().substr(m_cursor_in_buffer);
-					--m_cursor_in_buffer;
+					m_last_input = m_last_input.substr(0, m_cursor_in_input - 1)
+						+ m_last_input.substr(m_cursor_in_input);
+					--m_cursor_in_input;
 				}
 				break;
+			// enter
 			case 13:
-				m_cursor_in_buffer = 0;
-				m_buffer.back() += '\n';
-				m_text_parts.emplace_back(m_buffer.back(), m_main);
+				m_cursor_in_input = 0;
+				m_buffer.push_back(m_last_input);
+				m_last_input = "";
+				m_text_parts.emplace_back(m_buffer.back() + '\n', m_main);
 				start_new_command();
-				m_buffer.push_back("");
-				m_command_line = m_buffer.size() - 1;
+				m_command_line = m_buffer.size();
+				m_entered = true;
+				m_caninput = false;
 				break;
+			// left
 			case 37:
-				m_cursor_in_buffer = max(1, m_cursor_in_buffer) - 1;
+				m_cursor_in_input = max(1, m_cursor_in_input) - 1;
 				break;
+			// right
 			case 39:
-				m_cursor_in_buffer = min(m_cursor_in_buffer + 1, m_buffer.back().size());
+				m_cursor_in_input = min(m_cursor_in_input + 1, m_last_input.size());
 				break;
+			// up
 			case 38:
-				m_command_line = max(1, m_command_line) - 1;
-				m_buffer.back() = m_buffer[m_command_line];
-				m_cursor_in_buffer = m_buffer.back().size();
-				if (m_command_line != m_buffer.size() - 1)
+				if (m_current_input_type == TerminalInputType::COMMAND)
 				{
-					m_buffer.back().pop_back();
-					m_cursor_in_buffer--;
+					m_command_line = max(1, m_command_line) - 1;
+					m_last_input = m_buffer[m_command_line];
+					m_cursor_in_input = m_last_input.size();
 				}
 				break;
+			// down
 			case 40:
-				m_command_line = min(m_command_line + 1, m_buffer.size() - 1);
-				m_buffer.back() = m_buffer[m_command_line];
-				m_cursor_in_buffer = m_buffer.back().size();
-				if (m_command_line != m_buffer.size() - 1)
+				if (m_current_input_type == TerminalInputType::COMMAND && m_command_line != m_buffer.size())
 				{
-					m_buffer.back().pop_back();
-					m_cursor_in_buffer--;
+					m_command_line = min(m_command_line + 1, m_buffer.size() - 1);
+					m_last_input = m_buffer[m_command_line];
+					m_cursor_in_input = m_last_input.size();
 				}
 				break;
 			default:
 				break;
 			}
-
-			render_background();
-			render_text_end();
 		}
+
+		render_background();
+		render_text_end();
 	}
 	
 	void TerminalWindow::render_text_end() const
@@ -91,6 +133,16 @@ namespace GUI
 		load_screen_text().render_text_from(m_position, m_line_num);
 	}
 
+	void TerminalWindow::set_path(const Memory::FullPath& _path)
+	{
+		m_path = _path;
+	}
+
+	void TerminalWindow::set_user_name(const std::string& _name)
+	{
+		m_user = _name;
+	}
+	
 	void TerminalWindow::set_colours(Colours background, Colours main, Colours secondary, Colours third)
 	{
 		m_background = background;
@@ -104,11 +156,18 @@ namespace GUI
 	{
 		ScreenText stext(m_size);
 		stext.push_text(m_text_parts);
-		stext.push_text(m_buffer.back().substr(0, m_cursor_in_buffer), m_main);
-		if (m_cursor_in_buffer < m_buffer.back().size())
+		
+		std::string input;
+		if (m_current_input_type == TerminalInputType::PASSWORD)
+			input = std::string(m_last_input.size(), '*');
+		else
+			input = m_last_input;
+
+		stext.push_text(m_last_input.substr(0, m_cursor_in_input), m_main);
+		if (m_cursor_in_input < m_last_input.size())
 		{
-			stext.push_text(m_buffer.back()[m_cursor_in_buffer], m_selection);
-			stext.push_text(m_buffer.back().substr(m_cursor_in_buffer + 1), m_main);
+			stext.push_text(m_last_input[m_cursor_in_input], m_selection);
+			stext.push_text(m_last_input.substr(m_cursor_in_input + 1), m_main);
 		}
 		else
 			stext.push_text(" ", m_selection);
--- Artificial/TerminalWindow.h
+++ Artificial/TerminalWindow.h
@@ -5,30 +5,56 @@
 
 namespace GUI
 {
+	enum class TerminalInputType
+	{
+		COMMAND,
+		TEXT,
+		PASSWORD,
+	};
+
 	class TerminalWindow final : public BaseWindow
 	{
 	public:
 		TerminalWindow(const Memory::FullPath& _path, const std::string& _user, Size _size, ScreenPoint _position, const std::string& _title = "Window", Colours _background = Colours::BLACK);
 
+		void print_main(const std::string& line);
+		void print_secondary(const std::string& line);
+		void print_third(const std::string& line);
+		void print(const std::string& line, TextAttributes attributes);
+
+		void wait_for_input(TerminalInputType type);
+		std::string get_last_input();
 		virtual void key_pressed(KEY_EVENT_RECORD key_event) override;
 		virtual void render_text_end() const override;
 		virtual void render_text_line() const override;
+
+		void set_path(const Memory::FullPath& _path);
+		void set_user_name(const std::string& _name);
 	
 		void set_colours(Colours background, Colours main, Colours secondary, Colours third);
 	private:
 		ScreenText load_screen_text() const;
 		void start_new_command();
 
-		bool m_canprint = false;
-		std::vector<std::string> m_buffer;
+		std::string m_last_input;
+		bool m_entered = false;
+
 		Memory::FullPath m_path;
 		std::string m_user;
-		size_t m_cursor_in_buffer = 0;
+
+		TerminalInputType m_current_input_type = TerminalInputType::COMMAND;
+		bool m_caninput = false;
+
+		std::vector<std::string> m_buffer;
+		size_t m_cursor_in_input = 0;
 		size_t m_command_line = 0;
 
 		TextAttributes m_main = 15;
 		TextAttributes m_secondary = 6;
 		TextAttributes m_third = 14;
 		TextAttributes m_selection = 15 * 16;
+
+		std::mutex m_text_mutex;
+		std::condition_variable m_cv;
 	};
 }
