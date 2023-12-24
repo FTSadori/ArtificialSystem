f3ca95ec5d2fc8e5fea095f167895886c476b763
В'ячеслав <suzium.trap@gmail.com>
2023-12-24 16:06:08 +0200

    GUI: Added default constructor to ColourTheme structures

--- Artificial/ColourTheme.h
+++ Artificial/ColourTheme.h
@@ -5,20 +5,22 @@ namespace GUI
 {
 	struct SystemColourTheme final
 	{
-		Colours background;
-		Colours window;
-		Colours border;
+		Colours background = Colours::BLUE;
+		Colours window = Colours::BLACK;
+		Colours border = Colours::WHITE;
 
+		SystemColourTheme() {}
 		SystemColourTheme(Colours _background, Colours _window, Colours _border)
 			: background(_background), window(_window), border(_border) {}
 	};
 
 	struct TextColourTheme final
 	{
-		Colours main;
-		Colours secondary;
-		Colours third;
+		Colours main = Colours::WHITE;
+		Colours secondary = Colours::YELLOW;
+		Colours third = Colours::LIGHT_YELLOW;
 
+		TextColourTheme() {}
 		TextColourTheme(Colours _main, Colours _secondary, Colours _third)
 			: main(_main), secondary(_secondary), third(_third) {}
 	};
