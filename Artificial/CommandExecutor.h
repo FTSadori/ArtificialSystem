3d1f50d58965fbb376fccd0b92c4529658843065
В'ячеслав <suzium.trap@gmail.com>
2023-12-25 17:08:38 +0200

    Commands: Added mutex to CommandExecutor

--- Artificial/CommandExecutor.h
+++ Artificial/CommandExecutor.h
@@ -16,6 +16,7 @@ namespace Commands
 
 		virtual std::string execute(const ICommand& command, const User& sender) override
 		{
+			std::lock_guard lock(m_execution_mutex);
 			for (auto& controller : m_controllers)
 			{
 				if (controller.has_option(command.get("name")))
@@ -26,5 +27,6 @@ namespace Commands
 
 	protected:
 		std::vector<BaseController> m_controllers;
+		std::mutex m_execution_mutex;
 	};
 }
