3f187dfdf221eb7fb51cc23936dfa753bdc0328a
В'ячеслав <suzium.trap@gmail.com>
2024-02-02 14:29:54 +0200

    Mova: Added class for final module key words

---
+++ Artificial/FinalModuleKeyWords.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include <string>
+
+namespace Mova
+{
+	class FinalModuleKeyWords final
+	{
+	public:
+		static const std::vector<std::string> c_keywords;
+
+		static bool is_keyword(const std::string& word)
+		{
+			return (std::find(c_keywords.begin(), c_keywords.end(), word) != c_keywords.end());
+		}
+	};
+
+	const std::vector<std::string> FinalModuleKeyWords::c_keywords = { "while", "for", "func", "if", "end", "goto", };
+}
