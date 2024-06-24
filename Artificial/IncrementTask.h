73af64b5683f1f5d87d605326c6589ff5e96ef5a
В'ячеслав <suzium.trap@gmail.com>
2024-06-24 16:30:21 +0300

    Story: Added IncrementTask for future tests

---
+++ Artificial/IncrementTask.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "BaseTask.h"
+
+namespace Story
+{
+	class IncrementTask final : public BaseTask
+	{
+	public:
+		IncrementTask()
+			: BaseTask({{1.0}, {2.0}, {532.0}}, {{2.0}, {3.0}, {533.0}}) 
+		{}
+	};
+}
