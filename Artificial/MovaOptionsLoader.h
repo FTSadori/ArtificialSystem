83e985ba18e436ab27bccd5da4657295c1694343
В'ячеслав <suzium.trap@gmail.com>
2024-07-04 17:12:06 +0300

    Commands: Loaded new mova commands

--- Artificial/MovaOptionsLoader.h
+++ Artificial/MovaOptionsLoader.h
@@ -2,6 +2,8 @@
 #include "ICore.h"
 #include "BaseController.h"
 #include "RunProgramOption.h"
+#include "ShowOpenedTasksOption.h"
+#include "ShowTaskDetailsOption.h"
 
 namespace Commands
 {
@@ -12,6 +14,8 @@ namespace Commands
 		{
 			BaseController controller;
 			controller.add_option("mova", std::make_unique<RunProgramOption>(core));
+			controller.add_option("tasks", std::make_unique<ShowOpenedTasksOption>(core));
+			controller.add_option("showtask", std::make_unique<ShowTaskDetailsOption>(core));
 			return controller;
 		}
 	};
