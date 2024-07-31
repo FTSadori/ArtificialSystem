536119a979633abaa65461e472c4613219ec5dcd
В'ячеслав <suzium.trap@gmail.com>
2024-07-31 15:40:53 +0300

    Story: Included all scripts for chapter 2

--- Artificial/ItemUsingHandler.cpp
+++ Artificial/ItemUsingHandler.cpp
@@ -8,6 +8,23 @@ namespace Story
 		{ "[Glass key] OVERWORLD:\\largetree\\darkgorge3\\door\\blockeddoor", "CORE:\\.scripts\\chapter1\\u_key_2.txt" },
 		{ "[Weeping flower] OVERWORLD:\\largetree\\darkgorge3\\door", "CORE:\\.scripts\\chapter1\\u_wipfl_1.txt" },
 		{ "[Weeping flower] OVERWORLD:\\largetree\\darkgorge3\\door\\blockeddoor\\anotherdoor", "CORE:\\.scripts\\chapter1\\u_wipfl_2.txt" },
+		{ "[Glass fruit] GLASSTOWN:\\town\\lake", "CORE:\\.scripts\\chapter2\\d10_u_glassfruit.txt" },
+		{ "[Filled fruit] GLASSTOWN:\\town\\largebuilding", "CORE:\\.scripts\\chapter2\\d11_u_filledfruit.txt" },
+		{ "[Test tube] GLASSTOWN:\\roots\\deeper\\darkroots", "CORE:\\.scripts\\chapter2\\d20_u_testtube.txt" },
+		{ "[Filled test tube] GLASSTOWN:\\town\\largebuilding", "CORE:\\.scripts\\chapter2\\d21_u_filledtesttube.txt" },
+		{ "[Ilo sitelen] BLUE:\\tomosuli", "CORE:\\.scripts\\chapter2\\d22_u_ilositelen.txt" },
+		{ "[Glass knife] GLASSTOWN:\\town\\museum", "CORE:\\.scripts\\chapter2\\d55_u_glassknife.txt" },
+		{ "[Shiny perl] GLASSTOWN:\\roots\\deeper\\darkroots", "CORE:\\.scripts\\chapter2\\d56_u_perl.txt" },
+		{ "[Rare glass fruit] GLASSTOWN:\\town\\smallshop", "CORE:\\.scripts\\chapter2\\d58_u_rarefruit.txt" },
+		{ "[Amazing glass fruit] GLASSTOWN:\\town\\house", "CORE:\\.scripts\\chapter2\\d61_u_amazingfruit.txt" },
+		{ "[Packmi figurine] GLASSTOWN:\\town\\smallshop", "CORE:\\.scripts\\chapter2\\d62_u_packmifigurine.txt" },
+		{ "[Melodic bell] GLASSTOWN:\\town\\house", "CORE:\\.scripts\\chapter2\\d66_u_bell1.txt" },
+		{ "[Equations] GLASSTOWN:\\town\\largebuilding", "CORE:\\.scripts\\chapter2\\d69_u_mathtask.txt" },
+		{ "[Energetic bell] GLASSTOWN:\\town\\largebuilding", "CORE:\\.scripts\\chapter2\\d72_u_bell3.txt" },
+		{ "[Note] GLASSTOWN:\\town\\largebuilding", "CORE:\\.scripts\\chapter2\\d73_u_note.txt" },
+		{ "[Button flower] BLUE:\\tomosuli\\tomolili2", "CORE:\\.scripts\\chapter2\\d74_u_buttonflower.txt" },
+		{ "[Violet pencil] VIOLET:\\largeweb", "CORE:\\.scripts\\chapter2\\d77_u_purplepensil.txt" },
+		{ "[Strong bell] VIOLET:\\largeweb", "CORE:\\.scripts\\chapter2\\d79_u_bell2.txt" },
 	};
 
 	std::string ItemUsingHandler::get_script_for_event(const Memory::FullPath& position, const std::string& item_name)
--- Artificial/PackagesHandler.cpp
+++ Artificial/PackagesHandler.cpp
@@ -3,6 +3,11 @@
 namespace Story
 {
 	const std::unordered_map<std::string, std::string> PackagesHandler::s_map = {
-		{ "atb.pack", "BAZA:\\dialogs" }
+		{ "mova1.0.0.0.0.0.pack", "CORE:\\.scripts\\chapter2\\d3_ins_m1.txt" },
+		{ "nanoblessing.pack", "CORE:\\.scripts\\chapter2\\d5_ins_nano.txt" },
+		{ "mikiblessing.pack", "CORE:\\.scripts\\chapter2\\d6_ins_mk.txt" },
+		{ "tokipona.pack", "CORE:\\.scripts\\chapter2\\d75_ins_tokipona.txt" },
+		{ "remblessing.pack", "CORE:\\.scripts\\chapter2\\d82_ins_rem.txt" },
+		{ "elpablessing.pack", "CORE:\\.scripts\\chapter2\\d50_packmi.txt" },
 	};
 }
